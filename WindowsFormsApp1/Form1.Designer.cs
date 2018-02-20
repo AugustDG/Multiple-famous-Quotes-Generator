e12390145b18c42c35ca6f416df27929f6953d27
Augusto <augustomp55@gmail.com>
2018-02-19 22:11:10 -0500

    Fixes
    Fixing broken script files

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -28,12 +28,68 @@
     /// </summary>
     private void InitializeComponent()
     {
-      this.components = new System.ComponentModel.Container();
-      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-      this.Text = "Form1";
+      System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+      this.QuotesText = new System.Windows.Forms.Label();
+      this.generateButt = new System.Windows.Forms.Button();
+      this.SuspendLayout();
+      // 
+      // QuotesText
+      // 
+      this.QuotesText.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+      this.QuotesText.BackColor = System.Drawing.Color.Transparent;
+      this.QuotesText.CausesValidation = false;
+      this.QuotesText.Cursor = System.Windows.Forms.Cursors.Default;
+      this.QuotesText.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+      this.QuotesText.Font = new System.Drawing.Font("Quicksand Medium", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.QuotesText.Location = new System.Drawing.Point(137, 9);
+      this.QuotesText.Name = "QuotesText";
+      this.QuotesText.Size = new System.Drawing.Size(1443, 435);
+      this.QuotesText.TabIndex = 0;
+      this.QuotesText.Text = "[Quote] -[Author]";
+      this.QuotesText.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+      // 
+      // generateButt
+      // 
+      this.generateButt.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+      this.generateButt.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+      this.generateButt.FlatAppearance.BorderSize = 0;
+      this.generateButt.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+      this.generateButt.Font = new System.Drawing.Font("Comfortaa", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.generateButt.ForeColor = System.Drawing.SystemColors.HighlightText;
+      this.generateButt.Location = new System.Drawing.Point(8, 9);
+      this.generateButt.Name = "generateButt";
+      this.generateButt.Size = new System.Drawing.Size(114, 435);
+      this.generateButt.TabIndex = 1;
+      this.generateButt.Text = "Generate";
+      this.generateButt.UseVisualStyleBackColor = false;
+      this.generateButt.Click += new System.EventHandler(this.QuoteText_Click);
+      // 
+      // Form1
+      // 
+      this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+      this.BackgroundImage = global::Quotes.Properties.Resources.blue_orange_fog_banner_background_1_;
+      this.ClientSize = new System.Drawing.Size(1587, 456);
+      this.Controls.Add(this.generateButt);
+      this.Controls.Add(this.QuotesText);
+      this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+      this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+      this.MaximizeBox = false;
+      this.Name = "Form1";
+      this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+      this.Text = "Quotes";
+      this.Load += new System.EventHandler(this.LoadJson);
+      this.ResumeLayout(false);
+
     }
 
     #endregion
+
+    private System.Windows.Forms.Label QuotesText;
+    private System.Windows.Forms.Button generateButt;
   }
 }
 
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -1,20 +1,66 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Windows;
 using System.Windows.Forms;
+using Newtonsoft.Json;
 
 namespace WindowsFormsApp1
 {
   public partial class Form1 : Form
   {
+    public List<Quotes> QuotesList;
+    public List<int> QuotesNumbs = new List<int>();
+
     public Form1()
     {
       InitializeComponent();
+    }
+
+    public class Quotes
+    {
+      public string quoteAuthor { get; set; }
+      public string quoteText { get; set; }
+    }
+
+    public void LoadJson(object sender, EventArgs e)
+    {
+      using (StreamReader r = new StreamReader("quotesFile.json"))
+      {
+        string json = r.ReadToEnd();
+        QuotesList = JsonConvert.DeserializeObject<List<Quotes>>(json);
+      }
+    }
+
+    private void QuoteText_Click(object sender, EventArgs e)
+    {
+      if (generateButt.Text == "Restart")
+      {
+        QuotesNumbs.Clear();
+        generateButt.Text = "Generate";
+      }
+      Random random = new Random();
+      int rnd = 0;
+
+      while (QuotesNumbs.Exists(item => item == rnd))
+      {
+        rnd = random.Next(0, QuotesList.Count);
+        if (QuotesNumbs.Count == QuotesList.Count - 1)
+        {
+          QuotesText.Text = "Sorry, no more quotes (you did read all 1640 of them, right?)";
+          generateButt.Text = "Restart";
+          continue;
+        }
+      }
+
+      if (QuotesList[rnd].quoteAuthor != "")
+      {
+        QuotesText.Text = QuotesList[rnd].quoteText + " -" + QuotesList[rnd].quoteAuthor;
+      }else
+      {
+        QuotesText.Text = QuotesList[rnd].quoteText + " -Unknown";
+      }
+      QuotesNumbs.Add(rnd);
     }
   }
 }
